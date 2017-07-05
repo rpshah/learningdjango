06e57352c4302978832aedc745003b19d9b3f50a
rpshah <rpshah_96@yahoo.com>
2017-07-05 19:26:10 +0530

    learned unit testing with django

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question,Choice
 
@@ -13,13 +14,19 @@ class IndexView(generic.ListView):
     context_object_name = 'questions_list'
 
     def get_queryset(self):
-        """Return the first five published questions."""
-        return Question.objects.order_by('pub_date')[:5]
+        """Return the last five published questions, do not include future-relase questions"""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
