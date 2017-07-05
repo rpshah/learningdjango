5ae00f6f60c7e32830d4eec5e209db47c574758d
Gogs <gogs@fake.local>
2017-07-05 14:53:14 +0530

    request,response and forms handling and GenericViews learned

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
 admin.site.register(Question)
+admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,47 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question,Choice
 
-def index(request):
-	first_five_questions = Question.objects.order_by('pub_date')[:5] 
-	context = {'questions_list' : first_five_questions}
-	#return render(request, 'polls/index.html', context)
-	
-	template = loader.get_template('polls/index.html')
-	return HttpResponse(template.render(context,request)) 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'questions_list'
 
-def detail(request, question_id):
-	return HttpResponse("Displaying question %s" % question_id)
+    def get_queryset(self):
+        """Return the first five published questions."""
+        return Question.objects.order_by('pub_date')[:5]
 
-def results(request, question_id):
-	return HttpResponse("Poll stats for question %s" % question_id)
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-	return HttpResponse("Vote for question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 # Create your views here.
